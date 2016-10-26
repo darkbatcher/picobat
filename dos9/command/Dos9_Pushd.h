c85380fd6fd775c397770105dadfe7200eb7bed1
Astie Teddy <TSnake@Outlook.fr>
2016-10-26 17:02:43 +0200

    Rewrite Pushd/Popd commands.

--- dos9/command/Dos9_Pushd.h
+++ dos9/command/Dos9_Pushd.h
@@ -3,6 +3,7 @@
  *   Dos9 - A Free, Cross-platform command prompt - The Dos9 project
  *   Copyright (C) 2010-2016 Romain GARBI
  *   Copyright (C) 2016      Teddy ASTIE
+ *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
@@ -21,14 +22,7 @@
 #ifndef DOS9_CMD_PUSHD_H
 #define DOS9_CMD_PUSHD_H
 
-#define MAX_STACK_SIZE 1024
-
 int Dos9_CmdPushd(char *lpLine);
+int Dos9_CmdPopd (char *lpLine);
 
-// Directory stack specific
-void Dos9_Pushd_InitStack();
-void Dos9_Pushd_PushDirectory(char*);
-void Dos9_CmdPushd_PopDirectory();
-char* Dos9_Pushd_GetDirectory();
-
-#endif // DOS9_CMD_POPD_H
+#endif // DOS9_CMD_PUSHD_H
